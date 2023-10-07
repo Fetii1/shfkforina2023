9ff381d4f233ee44fcf53e210a5ea67f6a236682
Afan Ajdari <batman.two@hotmail.com>
2023-10-07 13:27:44 +0200

    refactor card section

--- components/sections/CardSection.js
+++ components/sections/CardSection.js
@@ -1,26 +1,31 @@
 import React from 'react'
+
 import Card from './Card'
 
 export default function CardSection() {
-  return (
-    <div className="w-auto mx-3 md:-mt-48 md:z-10 h-auto md:mx-10 md:space-x-4 flex flex-col justify-center  md:flex-row">
-      <Card
-        className="bg-blue-900 w-full h-52 rounded-3xl"
-        src="/svg/hat-image.svg"
-        text="Mësim Bashkëkohor"
-      />
-      <Card
-        className="bg-red-500 w-full h-52 rounded-3xl"
-        src="/svg/book-image.svg"
-        text="Bibliotekë"
-      />
-      <Card
-        className="bg-yellow-500 w-full h-52 rounded-3xl"
-        src="/svg/ball-image.svg"
-        text="Fushë Sporti
+  const items = [
+    {
+      src: '/svg/hat-image.svg',
+      text: 'Mësim Bashkëkohor',
+      className: 'bg-blue-500',
+    },
+    {
+      src: '/svg/book-image.svg',
+      text: 'Bibliotekë',
+      className: 'bg-red-500',
+    },
+    {
+      src: '/svg/ball-image.svg',
+      text: 'Fushë Sporti',
+      className: 'bg-yellow-500',
+    },
+  ]
 
-        "
-      />
+  return (
+    <div className="flex flex-col justify-center w-auto h-auto mx-3 md:-mt-48 md:z-10 md:mx-10 md:space-x-4 md:flex-row">
+      {items.map((item, index) => (
+        <Card key={index} src={item.src} text={item.text} className={item.className} />
+      ))}
     </div>
   )
 }
