bf6dfd62178bc96f4488c1e520c2b52e90a45b0e
Fetii1 <feti.kamberi@hotmail.com>
2023-10-09 14:16:06 +0200

    test commit

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '@/layout/Layout'
 export default function about() {
   return (
     <Layout title="About">
-      <h1>This is about page</h1>
+      <h1>This is about page test</h1>
     </Layout>
   )
 }
