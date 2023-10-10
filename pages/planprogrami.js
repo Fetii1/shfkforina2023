15cd9e5d4740cab7bb3e37cbae1af1b2b103222a
Fetii1 <feti.kamberi@hotmail.com>
2023-10-10 11:11:26 +0200

    Added HeaderSection

---
+++ pages/planprogrami.js
@@ -0,0 +1,10 @@
+import HeaderSection from '@/components/planprogrami/HeaderSection'
+import Layout from '@/layout/Layout'
+
+export default function Home() {
+  return (
+    <Layout title="Planprogrami">
+      <HeaderSection />
+    </Layout>
+  )
+}
