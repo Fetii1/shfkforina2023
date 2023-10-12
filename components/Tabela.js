3259da12f5e1183809d239a8c0850aa426165c50
Fetii1 <feti.kamberi@hotmail.com>
2023-10-12 14:34:30 +0200

    resolved issues

--- components/Tabela.js
+++ components/Tabela.js
@@ -3,38 +3,38 @@ import React from 'react'
 import Button from './Button'
 
 export default function Tabela() {
+  const classList = [
+    {
+      class: 'hover:bg-blue-950 transition ease-in-out duration-200 hover:-translate-y-1',
+    },
+  ]
+  const data = [
+    {
+      text: 'Plan Programi',
+    },
+    {
+      text: 'Vetëevaluimi',
+    },
+    {
+      text: 'Raport Vjetor',
+    },
+    {
+      text: 'програма',
+    },
+    {
+      text: 'Самоевалуација',
+    },
+    {
+      text: 'Годишна програма',
+    },
+  ]
+
   return (
     <div className="container">
-      <div className="w-full grid justify-items-center sm:grid-cols-2 md:grid-cols-3">
-        <Button
-          className={`hover:bg-blue-950 transition ease-in-out duration-200 hover:-translate-y-1 ${'w-fit my-2'}`}
-          text="Plan Programi"
-        />
-        <Button
-          className={`hover:bg-blue-950 transition ease-in-out duration-200 hover:-translate-y-1 ${'w-fit my-2'}`}
-          text="Vetëevaluimi"
-        />
-        <Button
-          className={`hover:bg-blue-950 transition ease-in-out duration-200 hover:-translate-y-1 ${'w-fit my-2'}`}
-          text="Raport Vjetor"
-        />
-      </div>
-      <div className="w-full grid justify-items-center sm:grid-cols-2 md:grid-cols-3">
-        <Button
-          Button
-          className={`hover:bg-blue-950 transition ease-in-out duration-200 hover:-translate-y-1 ${'w-fit my-2'}`}
-          text="програма"
-        />
-        <Button
-          Button
-          className={`hover:bg-blue-950 transition ease-in-out duration-200 hover:-translate-y-1 ${'w-fit my-2'}`}
-          text="Самоевалуација"
-        />
-        <Button
-          Button
-          className={`hover:bg-blue-950 transition ease-in-out duration-200 hover:-translate-y-1 ${'w-fit my-2'}`}
-          text="Годишна програма "
-        />
+      <div className="w-full grid justify-items-center sm:grid-cols-2 gap-5 md:grid-cols-3">
+        {data.map((btn, index) => (
+          <Button key={index} text={btn.text} className={classList[0].class} />
+        ))}
       </div>
     </div>
   )
