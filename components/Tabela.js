94c5639f9c46482f79abb622d081ef88c638bf9d
Fetii1 <feti.kamberi@hotmail.com>
2023-10-12 15:10:39 +0200

    removed class array

--- components/Tabela.js
+++ components/Tabela.js
@@ -3,11 +3,6 @@ import React from 'react'
 import Button from './Button'
 
 export default function Tabela() {
-  const classList = [
-    {
-      class: 'hover:bg-blue-950 transition ease-in-out duration-200 hover:-translate-y-1',
-    },
-  ]
   const data = [
     {
       text: 'Plan Programi',
@@ -33,7 +28,11 @@ export default function Tabela() {
     <div className="container">
       <div className="w-full grid justify-items-center sm:grid-cols-2 gap-5 md:grid-cols-3">
         {data.map((btn, index) => (
-          <Button key={index} text={btn.text} className={classList[0].class} />
+          <Button
+            key={index}
+            text={btn.text}
+            className={'hover:bg-blue-950 transition ease-in-out duration-200 hover:-translate-y-1'}
+          />
         ))}
       </div>
     </div>
