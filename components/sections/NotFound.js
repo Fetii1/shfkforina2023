a2a3411888369ed3a52df24abf59adcc3fbb3ed6
Fetii1 <feti.kamberi@hotmail.com>
2023-10-16 14:26:02 +0200

    added margin on notfound page

--- components/sections/NotFound.js
+++ components/sections/NotFound.js
@@ -5,15 +5,18 @@ import Button from '@/components/Button'
 
 export default function NotFound() {
   return (
-    <div className="container mt-24 mx-auto justify-around items-center flex flex-col lg:flex-row">
-      <div className="flex flex-col m-10 w-auto md:w-[455px]">
+    <div className="container my-28 mx-auto justify-around items-center flex flex-col lg:flex-row">
+      <div className="flex flex-col m-10 text-center md:text-left w-auto md:w-[455px]">
         <p className="text-3xl md:text-5xl font-bold">Faqja nuk u gjet!</p>
         <p className="my-4 md:my-6">
           Faqja që po kërkoni nuk ekziston. Ju mund të keni shkruar gabim adresën ose faqja mund të
           jetë zhvendosur.
         </p>
         <Link href="/">
-          <Button className={'w-fit'} text={'Kthehu mbrapa'} />
+          <Button
+            className="w-fit transition duration-150 hover:-translate-y-1 hover:bg-blue-950"
+            text={'Kthehu mbrapa'}
+          />
         </Link>
       </div>
       <div className="object-cover">
