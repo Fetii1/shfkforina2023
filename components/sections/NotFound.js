13c02697d29edc45f8b342cc57a9877580c69453
Fetii1 <feti.kamberi@hotmail.com>
2023-10-16 13:47:28 +0200

    added NotFound page, fixed navigation shadow

---
+++ components/sections/NotFound.js
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import Button from '@/components/Button'
+
+export default function NotFound() {
+  return (
+    <div className="container mt-24 mx-auto justify-around items-center flex flex-col lg:flex-row">
+      <div className="flex flex-col m-10 w-auto md:w-[455px]">
+        <p className="text-3xl md:text-5xl font-bold">Faqja nuk u gjet!</p>
+        <p className="my-4 md:my-6">
+          Faqja që po kërkoni nuk ekziston. Ju mund të keni shkruar gabim adresën ose faqja mund të
+          jetë zhvendosur.
+        </p>
+        <Link href="/">
+          <Button className={'w-fit'} text={'Kthehu mbrapa'} />
+        </Link>
+      </div>
+      <div className="object-cover">
+        <Image src="/img/404.png" width={550} height={500} alt="Not Found" />
+      </div>
+    </div>
+  )
+}
---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from '@/components/layout/Layout'
+import NotFound from '@/components/sections/NotFound'
+
+export default function Gallery() {
+  return (
+    <Layout title="Faqja Nuk u Gjet">
+      <NotFound />
+    </Layout>
+  )
+}
