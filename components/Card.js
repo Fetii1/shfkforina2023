003eb809d0730c6d916806f542f042a5aa9036a8
Fetii1 <feti.kamberi@hotmail.com>
2023-10-20 13:13:15 +0200

    fixed card height on md ss

--- components/Card.js
+++ components/Card.js
@@ -4,7 +4,7 @@ function Card({ className, src, text }) {
   return (
     <div className="w-full my-2 md:-mb-10 lg:-mb-16 xl:-mb-1 md:max-w-xs">
       <div className={`${className} z-50 rounded-2xl`}>
-        <div className="w-full h-72 flex flex-col items-center justify-evenly">
+        <div className="w-full h-56 lg:h-72 flex flex-col items-center justify-evenly">
           <SVG src={src} width={100} height={100} />
           <p className="text-lg px-5 text-center md:text-2xl lg:text-3xl font-bold text-white">
             {text}
