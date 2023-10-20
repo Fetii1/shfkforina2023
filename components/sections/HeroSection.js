676a1209be69c88f2562aa992567a27a3a4675ac
Fetii1 <feti.kamberi@hotmail.com>
2023-10-20 13:03:20 +0200

    fixed hero image on 4k

--- components/sections/HeroSection.js
+++ components/sections/HeroSection.js
@@ -2,21 +2,23 @@ import Image from 'next/image'
 
 export default function HeroSection() {
   return (
-    <div className="w-full relative">
-      <Image
-        className="absolute -bottom-1"
-        src="/img/curve.png"
-        width={1920}
-        height={1080}
-        alt="wave svg"
-      />
-      <Image
-        src="/img/hero-image.png"
-        width={1920}
-        height={1080}
-        alt="Hero Image"
-        priority={true}
-      />
+    <div className="w-screen">
+      <div className="w-full mx-auto relative">
+        <Image
+          className="absolute -bottom-1"
+          src="/img/curve.png"
+          width={2560}
+          height={1080}
+          alt="wave svg"
+        />
+        <Image
+          src="/img/hero-image.png"
+          width={2560}
+          height={1080}
+          alt="Hero Image"
+          priority={true}
+        />
+      </div>
     </div>
   )
 }
